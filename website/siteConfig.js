974daa0bc9c6ddc4913a079a47aaeb4710cd1bcf
emwuu <wu.emily23@gmail.com>
2020-10-02 12:17:48 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,7 +15,7 @@ const siteConfig = {
     title: 'Emily Wu', // Title for your website.
     tagline: 'Continuously improving, aspiring technologist.',
     profilePhoto: 'img/profile.png',
-    url: 'https:emwuu.github.io', // Your website URL
+    url: 'https://emwuu.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
